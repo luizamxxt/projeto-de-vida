e848b945c5de8b74f3d251e41021bb127958e6cf
luizamxxt <barbosa.almeida.maria@escola.pr.gov.br>
2026-05-07 12:36:33 -0300

    main

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const botoes = document.querySelectorAll(".botao")
 
 for(let botao = 0; botao < 4; botao++) {
     botoes[botao].onclick = function(){
-        botoes[botao].classList.add('ativo')
+
+        for(let outro = 0; outro < 4; outro++) {
+            botoes[outro].classList.remove ("ativo") 
+        }
+
+        botoes[botao].classList.add('ativo') 
     }
 }
